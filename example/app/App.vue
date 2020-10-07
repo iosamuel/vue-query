9864c5d65ac91e8d2598fa3f9fddc4257287d346
Miguel Ramos <miguelramos@nqda.pt>
2020-10-08 00:16:43 +0100

    Init build system for vuejs

---
+++ example/app/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      count: 0
+    }
+  },
+  setup(_props, _ctx) {
+    const showAlert = ref(true);
+
+    const log = (event: boolean) => showAlert.value = event;
+    const handleAlertVisibility = (_event: any) => {
+      showAlert.value = !showAlert.value;
+    }
+
+    return {
+      log,
+      showAlert,
+      handleAlertVisibility
+    }
+  }
+}
+</script>
